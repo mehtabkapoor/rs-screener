70508e5ea211e825c9da973bfaadf67227dee284
mehtabkapoor <mehtabkapoor@gmail.com>
2026-08-24 23:27:41 +0530

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -124,7 +124,7 @@ STCG_EFFECTIVE_RATE = STCG_RATE * (1 + STCG_CESS)
 SHEET_ID_ENV = "SHEET_ID"
 CREDS_ENV = "GOOGLE_CREDENTIALS"
 
-BACKTEST_WORKSHEET = "Backtest"
+BACKTEST_WORKSHEET = "Backtest - RS Top10 + Price TT + MM TT"
 
 
 # ============================================================
